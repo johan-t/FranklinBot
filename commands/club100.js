0473cbe520a5b4c2411712d84e6a62e9565c1dd9
Johan Trieloff <johan@trieloff.net>
2023-09-05 17:15:24 +0200

    checks the LHS

--- commands/club100.js
+++ commands/club100.js
@@ -3,32 +3,44 @@ const { github_token } = require('../config.json');
 
 async function handleClub100(interaction) {
     const dmChannel = await interaction.user.createDM();
-
     await dmChannel.send("Let's see if you are truly a Club 100 member. Please send me the link to one of your merged PRs that has a Lighthouse Score of 100. 👇");
 
-    // listen for user's response
     const filter = message => message.author.id === interaction.user.id;
-    const collector = dmChannel.createMessageCollector({ filter, time: 60000 });  // time in milliseconds, 1 minute
+    const collector = dmChannel.createMessageCollector({ filter, time: 60000 });
 
     collector.on('collect', async m => {
         console.log(`Collected ${m.content}`);
 
-        // Check if it's a GitHub PR link
         const isGithubPR = m.content.match(/^https:\/\/github.com\/([\w-]+)\/([\w-]+)\/pull\/(\d+)$/);
 
         if (isGithubPR) {
             const [_, owner, repo, pull_number] = isGithubPR;
 
             try {
-                const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/pulls/${pull_number}`, {
+                const prResponse = await axios.get(`https://api.github.com/repos/${owner}/${repo}/pulls/${pull_number}`, {
                     headers: {
                         'Authorization': `token ${github_token}`,
                         'Accept': 'application/vnd.github.v3+json'
                     }
                 });
 
-                if (response.data.merged === true) {
-                    await dmChannel.send("Congrats! You are a Club 100 member! 🎉");
+                if (prResponse.data.merged) {
+                    const commentsResponse = await axios.get(`https://api.github.com/repos/${owner}/${repo}/issues/${pull_number}/comments`, {
+                        headers: {
+                            'Authorization': `token ${github_token}`,
+                            'Accept': 'application/vnd.github.v3+json'
+                        }
+                    });
+
+                    const performance100Comment = commentsResponse.data.find(comment => {
+                        return comment.user.id === 43241697 && comment.body.includes('PERFORMANCE-100');
+                    });
+
+                    if (performance100Comment) {
+                        await dmChannel.send("Congrats! You truly are a Club 100 member! 💯");
+                    } else {
+                        await dmChannel.send("Sorry, couldn't find a performance score of 100. 😠");
+                    }
                 } else {
                     await dmChannel.send("This PR has not been merged. 😔");
                 }
